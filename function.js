dc4c2dc770b01fc4610b0c910c92e95fe1337c49
bruce5283 <bcarrington7@gmail.com>
2022-07-13 10:11:33 -0400

    Updated sound

--- function.js
+++ function.js
@@ -77,7 +77,7 @@ let sourceBuffer;
 
 const ctx = new AudioContext();
 
-fetch(url)
+fetch("./sounds/alarmsound.mp3")
   .then(response => response.arrayBuffer())
   .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
   .then(audioBuffer => {
